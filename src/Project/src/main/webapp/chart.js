86690faf433354e417d95fc53ad84bcb0ececcc8
Christophorus Jeremy Wicaksana <christophorusjeremywicaksana@student.utwente.nl>
2019-06-18 11:11:47 +0000

    compile sql counter and chart.js

--- src/Project/src/main/webapp/chart.js
+++ src/Project/src/main/webapp/chart.js
@@ -1,164 +1,4 @@
 $(document).ready(function() {
-  $("#generate").click(function() {
-    if (document.getElementById("radio1").checked) {
-      $('#container').replaceWith('<canvas id="container"></canvas>');
-      chart1();
-    } else if (document.getElementById("radio2").checked) {
-      $('#container').replaceWith('<canvas id="container"></canvas>');
-      chart2();
-    } else if (document.getElementById("radio3").checked) {
-      $('#container').replaceWith('<canvas id="container"></canvas>');
-      chart1();
-    } else if (document.getElementById("radio4").checked) {
-      $('#container').replaceWith('<canvas id="container" width= "800" height="400"></canvas>');
-      chart4();
-    }
-  })
-});
-
-
-
-function chart1(container) {
-  var ctx = document.getElementById('container').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-      datasets: [{
-        label: '# of Bookings',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
-  });
-};
-
-
-function chart2() {
-  var ctx = document.getElementById('container').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-      datasets: [{
-        label: '# of Bookings',
-        data: [20, 12, 9, 1, 16],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Top 5 Customers'
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-
-    }
-  });
-};
-
-function chart3() {
-  var ctx = document.getElementById('contain').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-      datasets: [{
-        label: '# of Bookings',
-        data: [20, 12, 9, 1, 16],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Top 5 Customers'
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-
-    }
-  });
-};
-
-function chart4() {
-  var ctx = document.getElementById('container').getContext('2d');
-  var myPieChart = new Chart(ctx, {
-    type: "doughnut",
-    data: {
-      labels: ["Train", "Truck", "Barge"],
-      datasets: [{
-        label: "# Containers per Mode of Transport",
-        data: [1910, 892, 2450],
-        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
-      }]
-    }
-  });
-}
-
-$(document).ready(function() {
   $('#orderStateFill').val("");
   $('#companyFill').val("");
   $('#locationFill').val("");
@@ -169,21 +9,8 @@ $(document).ready(function() {
 
 });
 
-
-
-
-function generateFilter() {
-  var orderState = document.getElementById("orderState1").value;
-  /*var bookingsper = document.getElementById("bookingsper").value;*/
-  var dateFrom = new Date($('#from').val());
-  var dateFromMilli = dateFrom.getTime() / 1000;
-  var dateTo = new Date($('#to').val());
-  var dateToMilli = dateTo.getTime() / 1000;
-  alert(dateFromMilli + " " + dateToMilli);
-  var output = "Order State: " + orderState + " From: " + dateFromMilli + " To: " + dateToMilli;
-
-  document.getElementById("testField").innerHTML = output;
-}
+var book;
+var weight;
 
 function chooseCheckBox() {
   if ($("#identificationCheck").is(":checked")) {
@@ -282,7 +109,37 @@ $(document).ready(function() {
     document.getElementById("teuButton").style.display = "none";
   });
 });
+function getCount() {
+	var http = new XMLHttpRequest();
+	var url = "http://localhost:8080/Project/rest/sql/count";
+	http.onreadystatechange = function() {
+		  if (this.readyState == 4 && this.status == 200) {
+			  
+			  document.getElementById("numcount").innerHTML = this.responseText;
+			  countit(); 
+		  }
+		};
+	http.open("GET", url);
+	http.send();
+};
 
+function getYear() {
+	var http = new XMLHttpRequest();
+	var url = "http://localhost:8080/Project/rest/sql/year";
+	http.onreadystatechange = function() {
+		  if (this.readyState == 4 && this.status == 200) {
+			  var temp = this.responseText.split("|");
+			  a = temp[0].split(";");
+			  b = temp[1].split(";").map(function(item) {
+				    return parseInt(item, 10);
+			  });
+			  chart404(a,b);
+		  }
+		};
+
+	http.open("GET", url);
+	http.send();
+};
 
 function getBookFilter(){
 	var http = new XMLHttpRequest();
@@ -305,6 +162,7 @@ function getBookFilter(){
 	var x
 	for(x in list ){
 		if(list[x] != ""){
+			
 			url += x + "=" + list[x];
 			if(url[url.length - 1] != "&"){
 				url += "&";
@@ -312,17 +170,15 @@ function getBookFilter(){
 		}
 		var newUrl = url.substring(0, url.length - 1);
 	}
-	alert(newUrl);
+	var text="";
 	http.onreadystatechange = function(){
 		  if (this.readyState == 4 && this.status == 200) {
-			  var text = this.responseText;
-			  alert(text);
+			  book = this.responseText;
 		  }
 	};
-
+	alert(book);
 	http.open("GET", url);
 	http.send();
-
 };
 
 function getBrutoFilter(){
@@ -346,70 +202,41 @@ function getBrutoFilter(){
 	var x
 	for(x in list ){
 		if(list[x] != ""){
-			url += x + "=" + list[x];
-			if(url[url.length - 1] != "&"){
-				url += "&";
+			if(x != list[list.length-1]){
+				url += x + "=" + list[x] + "&";
+			} else {
+				url += x + "=" + list[x];
 			}
 		}
 		var newUrl = url.substring(0, url.length - 1);
 	}
-	alert(newUrl);
+	var text="";
 	http.onreadystatechange = function(){
 		  if (this.readyState == 4 && this.status == 200) {
-			  var text = this.responseText;
-			  alert(text);
+			  weight = this.responseText;
 		  }
 	};
-
+	alert(weight);
 	http.open("GET", url);
 	http.send();
-
 };
 
-function getNettoFilter(){
-	var http = new XMLHttpRequest();
-	var dateFrom = new Date($('#dateFromFill').val());
-	var dateTo = new Date($('#dateToFill').val());
-	var fromD = "";
-	var toD = "";
-	if(dateFrom != "Invalid Date" && dateTo != "Invalid Date") {
-		fromD = dateFrom.getTime() / 1000;
-		toD = dateTo.getTime() / 1000;	
-	}
-	var dosId = $('#identificationFill').val();
-	var ordState = $('#orderStateFill').val();
-	var teu = $('#teuFill').val();
-	var shipComp= $('#companyNameFill').val();
-	var shipCompId = $('#companyIdFill').val();
-	var shipCompScac = $('#companyScacFill').val();
-	var list = {fromD, toD, dosId, ordState, teu, shipComp, shipCompId, shipCompScac};
-	var url = "http://localhost:8080/Project/rest/sql/nettoWeight?";
-	var x
-	for(x in list ){
-		if(list[x] != ""){
-			url += x + "=" + list[x];
-			if(url[url.length - 1] != "&"){
-				url += "&";
-			}
-		}
-		var newUrl = url.substring(0, url.length - 1);
-	}
-	alert(newUrl);
-	http.onreadystatechange = function(){
-		  if (this.readyState == 4 && this.status == 200) {
-			  var text = this.responseText;
-			  alert(text);
-		  }
-	};
 
-	http.open("GET", url);
-	http.send();
-
+function createTable(containTotal, weightTotal) {
+	var table ="<tr><td>Total Containers</td><td>Total Weight</td></tr><tr><td>" + containTotal + "</td><td>" + weightTotal + "</td></tr>";
+	return table;
 };
+
+function createResult(containTotal, weightTotal) {
+	var x = "Total Number of Containers: " + containTotal + "\n Total Weight: " + weightTotal;
+	return x;
+}
+	
 
 $(document).ready(function() {
   $("#search").click(function() {
-    getBookFilter();
-    //getBrutoFilter();
+	  getBrutoFilter();
+	  getBookFilter();
+	  document.getElementById('resultTab').innerHTML = createTable(book,weight);  
   })
 });
