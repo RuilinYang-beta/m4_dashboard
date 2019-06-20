c4acd9e4be711b96cdc6487ce91353488ffda08b
Arflyno Chrisdion P.P. Ludjen (Dion) <arflynochrisdionludjen@student.utwente.nl>
2019-06-20 08:50:22 +0000

    Update --> show graphs

--- src/Project/src/main/webapp/chart.js
+++ src/Project/src/main/webapp/chart.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#orderStateFill').val("");
+  $('#customFill').val("");
   $('#companyFill').val("");
   $('#locationFill').val("");
   $('#dateFromFill').val("");
@@ -9,106 +9,53 @@ $(document).ready(function() {
 
 });
 
-var book;
-var weight;
 
-function chooseCheckBox() {
-  if ($("#identificationCheck").is(":checked")) {
-    document.getElementById("identification").style.display = "block";
-    document.getElementById("identificationButton").style.display = "inline";
-  }
-  if ($("#companyCheck").is(":checked")) {
-    if($("#companyNameCheck").is(":checked")) {
-      document.getElementById("companyName").style.display = "block";
-      document.getElementById("companyId").style.display = "none";
-      document.getElementById("companyScac").style.display = "none";
-    } else if ($("#companyIdCheck").is(":checked")) {
-      document.getElementById("companyName").style.display = "none";
-      document.getElementById("companyId").style.display = "block";
-      document.getElementById("companyScac").style.display = "none";
-    } else {
-      document.getElementById("companyName").style.display = "none";
-      document.getElementById("companyId").style.display = "none";
-      document.getElementById("companyScac").style.display = "block";
+function maxLengthCheck(object, maxLength) {
+	if(object.value.length > maxLength) {
+        object.value = object.value.substr(0, maxLength);
     }
-	$('#companyNameFill').val('');
-    $('#companyIdFill').val('');
-    $('#companyScacFill').val('');
-    document.getElementById("companyButton").style.display = "inline";
   }
-  if ($("#locationCheck").is(":checked")) {
-    document.getElementById("location").style.display = "block";
-    document.getElementById("locationButton").style.display = "inline";
-  }
-  if ($("#orderStateCheck").is(":checked")) {
-    document.getElementById("orderState").style.display = "block";
-    document.getElementById("orderStateButton").style.display = "inline";
-  }
-  if ($("#transportCheck").is(":checked")) {
-    document.getElementById("transport").style.display = "block";
-    document.getElementById("transportButton").style.display = "inline";
-  }
-  if ($("#teuCheck").is(":checked")) {
-    document.getElementById("teu").style.display = "block";
-    document.getElementById("teuButton").style.display = "inline";
-  }
+
+function validate(evt) {
+	  var theEvent = evt || window.event;
+
+	  // Handle paste
+	  if (theEvent.type === 'paste') {
+	      key = event.clipboardData.getData('text/plain');
+	  } else {
+	  // Handle key press
+	      var key = theEvent.keyCode || theEvent.which;
+	      key = String.fromCharCode(key);
+	  }
+	  var regex = /[0-9]|\./;
+	  if( !regex.test(key) ) {
+	    theEvent.returnValue = false;
+	    if(theEvent.preventDefault) theEvent.preventDefault();
+	  }
+	}
+
+function check(divClass, checkboxClass){
+	var buttons = document.querySelector(divClass);
+	if (document.getElementById(checkboxClass).checked == true) {
+	    buttons.style.display = 'block';
+	} else {
+	    buttons.style.display = 'none';
+	}
 }
 
-$(document).ready(function() {
-  $("#add").click(function() {
-    chooseCheckBox();
-  })
-});
-
-function companyCheck() {
-  var buttons = document.querySelector('.companyChoice');
-  document.getElementById("companyNameCheck").checked = true;
-  if (document.getElementById("companyCheck").checked == true) {
-    buttons.style.display = 'block';
-  } else {
-    buttons.style.display = 'none';
-  }
-};
+function checkWithSelect(divClass, checkboxClass, fillInId){
+	var buttons = document.querySelector(divClass);
+	if (document.getElementById(checkboxClass).checked == true) {
+	    buttons.style.display = 'block';	
+	    $(fillInId).val(1);
+	} else {
+	    buttons.style.display = 'none';
+	    
+	}
+}
 
 
-$(document).ready(function() {
-  $("#identificationButton").click(function() {
-    $('#identificationFill').val('');
-    document.getElementById("identification").style.display = "none";
-    document.getElementById("identificationButton").style.display = "none";
-  });
-  $("#companyButton").click(function() {
-    $('#companyNameFill').val('');
-    $('#companyIdFill').val('');
-    $('#companyScacFill').val('');
-    document.getElementById("companyName").style.display = "none";
-    document.getElementById("companyId").style.display = "none";
-    document.getElementById("companyScac").style.display = "none";
-    document.getElementById("companyButton").style.display = "none";
-  });
-  $("#locationButton").click(function() {
-    $('#locationFill').val('');
-    document.getElementById("location").style.display = "none";
-    document.getElementById("locationButton").style.display = "none";
-  });
 
-  $("#orderStateButton").click(function() {
-    $('#orderStateFill').val('');
-    document.getElementById("orderState").style.display = "none";
-    document.getElementById("orderStateButton").style.display = "none";
-  });
-
-  $("#transportButton").click(function() {
-    $('#transportFill').val('');
-    document.getElementById("transport").style.display = "none";
-    document.getElementById("transportButton").style.display = "none";
-  });
-  $("#teuButton").click(function() {
-    $('#teuFill').val('');
-    document.getElementById("teu").style.display = "none";
-    document.getElementById("teuButton").style.display = "none";
-  });
-});
 function getCount() {
 	var http = new XMLHttpRequest();
 	var url = "http://localhost:8080/Project/rest/sql/count";
@@ -141,7 +88,12 @@ function getYear() {
 	http.send();
 };
 
-function getBookFilter(){
+
+var book;
+var weight;
+
+
+function getBookFilter(a){
 	var http = new XMLHttpRequest();
 	var dateFrom = new Date($('#dateFromFill').val());
 	var dateTo = new Date($('#dateToFill').val());
@@ -151,37 +103,61 @@ function getBookFilter(){
 		fromD = dateFrom.getTime() / 1000;
 		toD = dateTo.getTime() / 1000;	
 	}
-	var dosId = $('#identificationFill').val();
+	var dosId = $('#customFill').val();
 	var ordState = $('#orderStateFill').val();
 	var teu = $('#teuFill').val();
 	var shipComp= $('#companyNameFill').val();
-	var shipCompId = $('#companyIdFill').val();
-	var shipCompScac = $('#companyScacFill').val();
+	var shipCompId = "";
+	var shipCompScac = "";
 	var list = {fromD, toD, dosId, ordState, teu, shipComp, shipCompId, shipCompScac};
-	var url = "http://localhost:8080/Project/rest/sql/select?";
-	var x
-	for(x in list ){
-		if(list[x] != ""){
-			
-			url += x + "=" + list[x];
-			if(url[url.length - 1] != "&"){
-				url += "&";
+	var url = "http://localhost:8080/Project/rest/sql/select?goal=bookings&";
+	var x;
+	if(a == "table"){
+		url += "table=true&";
+		for(x in list ){
+			if(list[x] != ""){
+				if(x != list[list.length - 1]){
+					url += x + "=" + list[x] + "&";
+				}
 			}
+			var newUrl = url.substring(0, url.length - 1);
 		}
-		var newUrl = url.substring(0, url.length - 1);
+		
+		http.onreadystatechange = function(){
+			  if (this.readyState == 4 && this.status == 200) {
+				  book = this.responseText;
+				  
+				  document.getElementById('resultTab').innerHTML = createTable(book,weight);  
+			  }
+		};
+		
+	}else{
+		for(x in list ){
+			if(list[x] != ""){
+				if(x != list[list.length - 1]){
+					url += x + "=" + list[x] + "&";
+				}
+			}
+			var newUrl = url.substring(0, url.length - 1);
+		}
+		
+		http.onreadystatechange = function(){
+			  if (this.readyState == 4 && this.status == 200) {
+				  var temp = this.responseText.split("|");
+				  a = temp[0].split(";");
+				  b = temp[1].split(";");
+				  alert(a);
+				  alert(b);
+				  lineChart(a,b,"container");
+			  }
+		};
 	}
-	var text="";
-	http.onreadystatechange = function(){
-		  if (this.readyState == 4 && this.status == 200) {
-			  book = this.responseText;
-		  }
-	};
-	alert(book);
+	
 	http.open("GET", url);
 	http.send();
 };
 
-function getBrutoFilter(){
+function getBrutoFilter(a){
 	var http = new XMLHttpRequest();
 	var dateFrom = new Date($('#dateFromFill').val());
 	var dateTo = new Date($('#dateToFill').val());
@@ -191,39 +167,46 @@ function getBrutoFilter(){
 		fromD = dateFrom.getTime() / 1000;
 		toD = dateTo.getTime() / 1000;	
 	}
-	var dosId = $('#identificationFill').val();
+	var dosId = $('#customFill').val();
 	var ordState = $('#orderStateFill').val();
 	var teu = $('#teuFill').val();
 	var shipComp= $('#companyNameFill').val();
-	var shipCompId = $('#companyIdFill').val();
-	var shipCompScac = $('#companyScacFill').val();
+	var shipCompId = "";
+	var shipCompScac = "";
 	var list = {fromD, toD, dosId, ordState, teu, shipComp, shipCompId, shipCompScac};
-	var url = "http://localhost:8080/Project/rest/sql/brutoWeight?";
+	var url = "http://localhost:8080/Project/rest/sql/select?goal=brutoWeight&";
 	var x
+	if(a == "table"){
+		url += "table=true&";
+	}else{
+		url;
+	}
 	for(x in list ){
 		if(list[x] != ""){
 			if(x != list[list.length-1]){
 				url += x + "=" + list[x] + "&";
-			} else {
-				url += x + "=" + list[x];
-			}
+			} 
 		}
+	
 		var newUrl = url.substring(0, url.length - 1);
+		
 	}
-	var text="";
+	
+	
 	http.onreadystatechange = function(){
 		  if (this.readyState == 4 && this.status == 200) {
 			  weight = this.responseText;
+			  document.getElementById('resultTab').innerHTML = createTable(book,weight);  
 		  }
 	};
-	alert(weight);
-	http.open("GET", url);
+	
+	http.open("GET", newUrl);
 	http.send();
 };
 
 
 function createTable(containTotal, weightTotal) {
-	var table ="<tr><td>Total Containers</td><td>Total Weight</td></tr><tr><td>" + containTotal + "</td><td>" + weightTotal + "</td></tr>";
+	var table ="<tr><th>Total Containers</th><td>" + containTotal + "</td></tr><tr><th>Total Weight</th><td>" + weightTotal + "</td></tr>";
 	return table;
 };
 
@@ -231,12 +214,62 @@ function createResult(containTotal, weightTotal) {
 	var x = "Total Number of Containers: " + containTotal + "\n Total Weight: " + weightTotal;
 	return x;
 }
-	
+
+function combine(){
+	getBrutoFilter();
+	getBookFilter();
+	getBookFilter("table");
+
+}
 
 $(document).ready(function() {
-  $("#search").click(function() {
-	  getBrutoFilter();
-	  getBookFilter();
-	  document.getElementById('resultTab').innerHTML = createTable(book,weight);  
+  $("#search").click(function() { 
+	  combine();
+	  $('#container').replaceWith('<canvas id="container"></canvas>');
   })
+  
 });
+
+
+////// Create Charts
+
+
+function lineChart(year, total, canvas) {
+	  var ctx = document.getElementById(canvas).getContext('2d');
+	  var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: year,
+	        datasets: [{
+	            label: '# of Containers',
+	            data: total,
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	  });
+};
